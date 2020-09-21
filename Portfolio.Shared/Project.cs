d8aafbfc75055e26fe496bbf448724c540fbed19
Stuart <nielsen.stuart9@gmail.com>
2020-09-21 14:32:14 -0600

    Add project files.

---
+++ Portfolio.Shared/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Shared
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Requirements { get; set; }
+    }
+}
