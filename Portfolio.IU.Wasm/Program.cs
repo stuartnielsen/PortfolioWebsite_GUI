39b6c9a6e987227a7f11c7d2547649ab0786c687
Stuart <nielsen.stuart9@gmail.com>
2020-09-22 12:56:36 -0600

    ProjectDetails navigation working

--- Portfolio.IU.Wasm/Program.cs
+++ Portfolio.IU.Wasm/Program.cs
@@ -17,7 +17,7 @@ namespace Portfolio.IU.Wasm
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5005") });
 
             await builder.Build().RunAsync();
         }
